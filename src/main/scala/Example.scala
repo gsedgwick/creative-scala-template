f2bfa98478b33ae675973797adec85825f6a746d
am_dev <>
2017-03-29 17:59:08 -0400

    aaaa

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,12 +1,10 @@
-import doodle.core._
 import doodle.core.Image._
-import doodle.syntax._
-import doodle.jvm.Java2DCanvas._
-import doodle.backend.StandardInterpreter._
 
 // To use this example, open the SBT console and type:
 //
 // Example.image.draw
 object Example {
-  val image = circle(10) on circle(20) on circle(30)
+  val image = circle(10) on circle(20) on circle(100)
+  //Test//
+
 }
