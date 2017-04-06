397a9e5a50ebbed0f5c1992d4a2dd4deb39d9c11
gsedgwick <gs996@yahoo.com>
2017-04-05 23:43:13 -0400

    kix

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -8,5 +8,5 @@ object Example {
   val image = circle(10) on circle(20) on circle(100)
   //Test//
 val image2 = ((circle(20) fillColor (Color.red)) beside circle (20) beside circle (20)) on circle (300)
-
+val image3 =  ((circle(200) fillColor (Color.red)) beside circle (20) beside circle (20)) on circle (300)
 }
